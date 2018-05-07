4eb2c94e2c78971bf8f4247e7dd1248c15a37339
Ajay Reddy Yeruva <ajayreddyyeruva@gmail.com>
2018-05-06 19:39:46 -0700

    Update build.sh

--- cpp-ci-cd-example/docker-static-binary/build.sh
+++ cpp-ci-cd-example/docker-static-binary/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-echo 0. Remove existing hello-world binary in workspace
-rm ${WORKSPACE}/cpp-ci-cd-example/bazel-bin/main/hello-world
-
 echo 1. Building our run environment:
 docker build run -f run/Dockerfile -t hello-world:1.0
 echo -e "\n"
 
 echo 2. Running our runtime environment:
-docker run --rm hello-world:1.0
+docker run --rm hello-world:1.0 /bin/bash -c "/hello-world"
 echo -e "\n"
 
 echo 3. Binary size:
