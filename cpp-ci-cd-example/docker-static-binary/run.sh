68a8e13ee6825d7e8c24817cf08f8be997bad0d2
Ajay Reddy Yeruva <ajayreddyyeruva@gmail.com>
2018-05-06 19:52:37 -0700

    Update run.sh

--- cpp-ci-cd-example/docker-static-binary/run.sh
+++ cpp-ci-cd-example/docker-static-binary/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo 1. Building our run environment:
-docker build run -f run/Dockerfile -t hello-world:1.0
+/usr/local/bin/docker build run -f run/Dockerfile -t hello-world:1.0
 echo -e "\n"
 
 echo 2. Running our runtime environment:
-docker run --rm hello-world:1.0 /bin/bash -c "/hello-world"
+/usr/local/bin/docker run --rm hello-world:1.0 /bin/bash -c "/hello-world"
 echo -e "\n"
 
 echo 3. Binary size:
