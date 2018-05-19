b68983a73466977e6c6c49eb85cda90e1c752757
Ajay Reddy Yeruva <ajayreddyyeruva@gmail.com>
2018-05-18 22:58:29 -0700

    Update run.sh

--- cpp-ci-cd-example/docker-static-binary/run.sh
+++ cpp-ci-cd-example/docker-static-binary/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo 1. Building our run environment:
-/usr/local/bin/docker build run -f run/Dockerfile -t mindstreamorg/hello-world:1.0
+/usr/bin/docker build run -f run/Dockerfile -t mindstreamorg/hello-world:1.0
 echo -e "\n"
 
 echo 2. Pushing the docker image to Dockerhub:
-/usr/local/bin/docker push mindstreamorg/hello-world:1.0
+/usr/bin/docker push mindstreamorg/hello-world:1.0
 echo -e "\n"
 
 echo 2. Running our runtime environment:
-/usr/local/bin/docker run --rm mindstreamorg/hello-world:1.0 sh -c "echo Hello World"
+/usr/bin/docker run --rm mindstreamorg/hello-world:1.0 sh -c "echo Hello World"
 echo -e "\n"
 
 echo 3. Binary size:
