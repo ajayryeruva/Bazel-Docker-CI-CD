1f66a63c98cb5fcad592bb8f21c5596b89a2e0b5
AjayRY <20268787+ajayreddy1332@users.noreply.github.com>
2018-05-19 21:16:49 -0700

    Update run.sh

--- cpp-ci-cd-example/docker-static-binary/run.sh
+++ cpp-ci-cd-example/docker-static-binary/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo 1. Building our run environment:
-/usr/bin/docker build run -f run/Dockerfile -t mindstreamorg/hello-world:1.0
+sudo /usr/bin/docker build run -f run/Dockerfile -t mindstreamorg/hello-world:1.0
 echo -e "\n"
 
 echo 2. Pushing the docker image to Dockerhub:
-/usr/bin/docker push mindstreamorg/hello-world:1.0
+sudo /usr/bin/docker push mindstreamorg/hello-world:1.0
 echo -e "\n"
 
 echo 2. Running our runtime environment:
 #/usr/bin/docker run --rm mindstreamorg/hello-world:1.0 sh -c "echo Hello World"
-/usr/bin/docker run --rm mindstreamorg/hello-world:1.0
+sudo /usr/bin/docker run --rm mindstreamorg/hello-world:1.0
 echo -e "\n"
 
 echo 3. Binary size:
-ls -lrth ${WORKSPACE}/cpp-ci-cd-example/bazel-bin/main/hello-world
+sudo ls -lrth ${WORKSPACE}/cpp-ci-cd-example/bazel-bin/main/hello-world
