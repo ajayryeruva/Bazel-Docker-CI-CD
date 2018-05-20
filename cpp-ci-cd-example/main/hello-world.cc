64efe18e838a172119bdf861aad668386211f329
AjayRY <20268787+ajayreddy1332@users.noreply.github.com>
2018-05-19 19:08:40 -0700

    Update hello-world.cc

--- cpp-ci-cd-example/main/hello-world.cc
+++ cpp-ci-cd-example/main/hello-world.cc
@@ -1,7 +1,14 @@
-#include <stdio.h>
+#include "lib/hello-time.h"
+#include "main/hello-greet.h"
+#include <iostream>
+#include <string>
 
-int main()
-{
-  printf("Hello World!");
+int main(int argc, char** argv) {
+  std::string who = "world";
+  if (argc > 1) {
+    who = argv[1];
+  }
+  std::cout << get_greet(who) << std::endl;
+  print_localtime();
   return 0;
 }
