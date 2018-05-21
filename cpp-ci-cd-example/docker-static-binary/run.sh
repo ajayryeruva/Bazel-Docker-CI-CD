4546dfd29f15362a15a6ac00927f13de10b1fa8e
tat08507 <tat08507@adobe.com>
2018-05-20 20:07:42 -0700

    Docker incremental version

--- cpp-ci-cd-example/docker-static-binary/run.sh
+++ cpp-ci-cd-example/docker-static-binary/run.sh
@@ -5,7 +5,7 @@ sudo /usr/bin/docker build run -f run/Dockerfile -t mindstreamorg/hello-world:1.
 echo -e "\n"
 
 echo 2. Tag Docker image:
-sudo docker tag mindstreamorg/hello-world:1.0 mindstreamorg/hello-world:$(git rev-parse --short HEAD)
+sudo /usr/bin/docker tag mindstreamorg/hello-world:1.0 mindstreamorg/hello-world:$(git rev-parse --short HEAD)
 echo -e "\n"
 
 echo 3. Pushing the docker image to Dockerhub:
@@ -13,7 +13,7 @@ sudo /usr/bin/docker push mindstreamorg/hello-world:$(git rev-parse --short HEAD
 echo -e "\n"
 
 echo 4. Running our runtime environment:
-sudo docker run --rm mindstreamorg/hello-world:$(git rev-parse --short HEAD) /bin/sh -c "./hello-world"
+sudo /usr/bin/docker run --rm mindstreamorg/hello-world:$(git rev-parse --short HEAD) /bin/sh -c "./hello-world"
 echo -e "\n"
 
 echo 5. Binary size:
@@ -21,9 +21,9 @@ sudo ls -lrth ${WORKSPACE}/cpp-ci-cd-example/bazel-bin/main/hello-world
 echo -e "\n"
 
 echo 6. Cleanup Docker containers:
-sudo docker rm -f $(sudo docker ps -a)
+sudo /usr/bin/docker rm -f $(sudo docker ps -a)
 echo -e "\n"
 
 echo 7. Cleanup Docker images:
-sudo docker rmi -f $(sudo docker images)
+sudo /usr/bin/docker rmi -f $(sudo docker images)
 echo -e "\n"
